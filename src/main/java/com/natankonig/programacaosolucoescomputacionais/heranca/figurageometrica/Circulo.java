b9c767cf0beb329131bb810546fd44e31a9d9610
Natan Konig <natan.camargo8@gmail.com>
2025-04-14 09:33:46 -0300

    add example of extends

---
+++ src/main/java/com/natankonig/programacaosolucoescomputacionais/heranca/figurageometrica/Circulo.java
@@ -0,0 +1,36 @@
+package com.natankonig.programacaosolucoescomputacionais.heranca.figurageometrica;
+
+import javax.swing.JOptionPane;
+
+public class Circulo extends FiguraGeometrica {
+
+    private double raio;
+
+    public Circulo() {
+        this("",0);
+    }
+    public Circulo(String cor, double raio) {
+        super(cor);
+        setRaio(raio);
+    }
+    public double getRaio() {
+        return raio;
+    }
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+    public void leitura() {
+        super.leitura();
+        setRaio(Double.parseDouble(JOptionPane.showInputDialog("Digite o raio")));
+    }
+    public void imprimir() {
+        super.imprimir();
+        JOptionPane.showMessageDialog(null,"O raio é " + getRaio());
+    }
+    public String paraString() {
+        return (super.paraString() + "/ Raio : " + getRaio());
+    }
+    public double getArea() {
+        return (Math.PI * getRaio() * getRaio());
+    }
+}
