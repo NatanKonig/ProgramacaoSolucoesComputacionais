0df64ae19e12dd6a60deb85cf090e79f668f074d
Natan Konig <natan.camargo8@gmail.com>
2025-05-12 10:31:47 -0300

    exercise Triangulo with interface

---
+++ src/main/java/com/natankonig/programacaosolucoescomputacionais/interfaces/ExemploFrmTriangulo/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.natankonig.programacaosolucoescomputacionais.interfaces.ExemploFrmTriangulo;
+
+import com.natankonig.programacaosolucoescomputacionais.interfaces.ExemploFrmTriangulo.view.FrmTriangulo;
+
+/**
+ *
+ * @author natan
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        FrmTriangulo janela = new FrmTriangulo();
+        janela.setVisible(true);
+        
+    }
+}
